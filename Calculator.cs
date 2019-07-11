168e2409487cec121f665b3e5144c2f57239608a
dek-haji <deeeqhaji@gmail.com>
2019-07-10 20:25:30 -0500

    codded along with Jissie

---
+++ Calculator.cs
@@ -0,0 +1,12 @@
+
+namespace Exceptions
+{
+    public class Calculator
+    {
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        // ...other methods (e.g. Add(), Subtract(), Multiply())...
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company chickenShack = new Company() { Name = "Greasy Pete's Chicken Shack" };
+            chickenShack.AddEmployee(new Employee() { FirstName = "Pete", LastName = "Shackleton" });
+            chickenShack.AddEmployee(new Employee() { FirstName = "Molly", LastName = "Frycook" });
+            chickenShack.AddEmployee(new Employee() { FirstName = "Pat", LastName = "Buttersmith" });
+
+            chickenShack.AddEmployee(new Employee() {FirstName = "Dek", LastName = "Haji"});
+
+            List<int> employeeIds = new List<int>() { 0, 11, 2, 3 };
+            foreach (int id in employeeIds)
+            {
+                try
+                {
+                    Employee employee = chickenShack.GetEmployeeById(id);
+                    Console.WriteLine($"Employee #{id} is {employee.FirstName} {employee.LastName}.");
+
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine($"Employee #{id} was not found in the company.because of {ex.Message}");
+                }
+            }
+        }
+    }
+}
